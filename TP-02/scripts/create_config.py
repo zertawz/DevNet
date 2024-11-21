da01d94116d6fc82a7cdd2fd8987d0d33f9ea4ad
VM-archlinux <Vous@exemple.com>
2024-11-21 22:59:12 +0100

    TP2 almost done

--- TP-02/scripts/create_config.py
+++ TP-02/scripts/create_config.py
@@ -2,40 +2,52 @@ import json
 from jinja2 import Template, Environment,  FileSystemLoader
 
 env = Environment(loader=FileSystemLoader("templates"))
+#print(env.list_templates())
 
 def load_json_data_from_file(file_path):
-    pass
+    with open(file_path, 'r') as variables:
+        data = json.load(variables)
+    return data
 
 def render_network_config(template_name, data):
-    pass
+    template = env.get_template(template_name)
+    return template.render(data)
 
 
 def save_built_config(file_name, data):
-    pass
-
+    with open(file_name, 'w') as f:
+        f.write(data)
 
 def create_vlan_config_cpe_marseille():
     """
         Must return two values : router config and the switch config
     """
-    pass
+    r02_data = load_json_data_from_file(file_path='data/vlan_R02.json')
+    esw2_data = load_json_data_from_file(file_path='data/vlan_ESW2.json')
+    r02_config = render_network_config(template_name='vlan_router.j2', data=r02_data)
+    esw2_config = render_network_config(template_name='vlan_switch.j2', data=esw2_data)
+    return r02_config, esw2_config
 
 
 def create_vlan_config_cpe_paris():
     """
         Must return two values : router config and the switch config
     """
-    pass
+    r03_data = load_json_data_from_file(file_path='data/vlan_R03.json')
+    esw3_data = load_json_data_from_file(file_path='data/vlan_ESW3.json')
+    r03_config = render_network_config(template_name='vlan_router.j2', data=r03_data)
+    esw3_config = render_network_config(template_name='vlan_switch.j2', data=esw3_data)
+    return r03_config, esw3_config
 
 
 if __name__ == "__main__":
     """
         process question 1 to 5:
     """
-    # r02_config, esw2_config = create_vlan_config_cpe_marseille()
-    # save_built_config('config/vlan_R02.conf', r02_config)
-    # save_built_config('config/vlan_ESW2.conf', esw2_config)
+    r02_config, esw2_config = create_vlan_config_cpe_marseille()
+    save_built_config('config/vlan_R02.conf', r02_config)
+    save_built_config('config/vlan_ESW2.conf', esw2_config)
     
-    # r03_config, esw3_config = create_vlan_config_cpe_paris()
-    # save_built_config('config/vlan_R03.conf', r03_config)
-    # save_built_config('config/vlan_ESW3.conf', esw3_config)
+    r03_config, esw3_config = create_vlan_config_cpe_paris()
+    save_built_config('config/vlan_R03.conf', r03_config)
+    save_built_config('config/vlan_ESW3.conf', esw3_config)
--- TP-02/scripts/run_netmiko.py
+++ TP-02/scripts/run_netmiko.py
@@ -2,67 +2,103 @@ import json
 from netmiko import ConnectHandler
 
 def question_9(net_connect):
-    pass
+    print(net_connect)
+    print(net_connect.__dict__)
+    print(f"Adresse IP : {net_connect.host}")
+    print(f"Type d'appareil : {net_connect.device_type}")
 
 def question_10(net_connect):
-    pass
+    output = net_connect.send_command("show ip int brief")
+    print(output)
 
 def question_11(net_connect):
-    pass
+    output = net_connect.send_command("show ip int brief",use_textfsm=True)
+    print(output)
+    print(type(output))
 
 
 def question_12(net_connect):
-    pass
+    output = net_connect.send_command("sh ip route",use_textfsm=True)
+    print(output)
+    print(type(output))
 
 def question_13(net_connect):
-    pass
+    output = net_connect.send_command("show ip int brief",use_textfsm=True)
+    interfaces = []
+    for item in output:
+        #get a list with the interfaces (maybe usefull later)
+        interfaces.append(item['interface'])
+        suboutput = net_connect.send_command(f"sh run | s {item['interface']}")
+        print(suboutput)
 
 
 def question_14(net_connect):
-    pass
+    commands = [
+        'interface loopback 1',
+        'ip address 192.168.1.1 255.255.255.255',
+        'description loopback interface from netmiko'
+        ]
+    net_connect.send_config_set(commands)
 
 
 def question_15(net_connect):
-    pass
+    commands = [
+        'no interface loopback 1',
+        'no ip address 192.168.1.1 255.255.255.255',
+        'no description loopback interface from netmiko'
+        ]
+    net_connect.send_config_set(commands)
 
 def question_16(net_connect):
-    pass
+    output = net_connect.send_config_from_file("config/loopback_R01.conf")
+    #print(output)
+
 
 
 def question_17(net_connect):
-    pass
+    output = net_connect.send_config_from_file("config/no_loopback_R01.conf")
 
 
 def get_inventory():
-    pass
+    with open("data/hosts.json", 'r') as hosts:
+        data = json.load(hosts)
+    return data
 
 
 def question_20():
-    pass
+    hosts = get_inventory()
+    for host in hosts:
+        print(host)
+        if host["hostname"][0].lower()=="r":
+            net_connect2 = ConnectHandler(**host)
+            net_connect2.send_command(f"sh run | s interface g0/0.99")
+            net_connect2.disconnect()
 
 
 def question_21():
     pass
 
 if __name__ == "__main__":    
+
     r01 = {
         'device_type': 'cisco_ios',
-        'host':   'xx.xx.xx.xx',
-        'username': 'xx',
-        'password': 'xx'
+        'host':   '172.16.100.126',
+        'username': 'cisco',
+        'password': 'cisco',
+        'port': 22
     }
-    #net_connect = ConnectHandler(**r01)
+    net_connect = ConnectHandler(**r01)
     
-    #question_9(net_connect)
-    #question_10(net_connect)
-    #question_11(net_connect)
-    #question_12(net_connect)
-    #question_13(net_connect)
-    #question_14(net_connect)
-    #question_15(net_connect)
-    #question_16(net_connect)
-    #question_17(net_connect)
-    # hosts = get_inventory()
-    # print(hosts)
-    #question_20()
+    question_9(net_connect)
+    question_10(net_connect)
+    question_11(net_connect)
+    question_12(net_connect)
+    question_13(net_connect)
+    question_14(net_connect)
+    question_15(net_connect)
+    question_16(net_connect)
+    question_17(net_connect)
+    hosts = get_inventory()
+    print(hosts)
+    question_20()
     #question_21()
