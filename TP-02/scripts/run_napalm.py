e5001f134ef7a83e3170a79bb24ade5a858a1092
VM-archlinux <Vous@exemple.com>
2024-11-24 16:26:47 +0100

    tp2 finished need debug ssh

--- TP-02/scripts/run_napalm.py
+++ TP-02/scripts/run_napalm.py
@@ -1,6 +1,8 @@
 import json
 from napalm import get_network_driver
+from jinja2 import Template, Environment,  FileSystemLoader
 
+env = Environment(loader=FileSystemLoader("templates"))
 
 def get_inventory():
     pass
@@ -37,17 +39,47 @@ def question_30(device):
     print(output["show ip int brief"])
 
 def question_31():
-    with open(file_path, 'r') as variables:
+    with open("data/ospf.json", 'r') as variables:
         data = json.load(variables)
+        variables.close()
+    template = env.get_template("ospf.j2")
+    for router in data:
+        conf = template.render(router)
+        with open(f"config/ospf_{router["hostname"].lower()}.conf", 'w') as f:
+            f.write(conf)
+            f.close()
+
 
 
 
 def question_32():
-    pass
+    with open("data/hosts.json", 'r') as hosts:
+        data = json.load(hosts)
+    for host in data:
+        if "r" in host["hostname"].lower():
+            driver = get_network_driver('ios')
+            device  = driver(hostname=host["ip"], username=host["username"], password=host["password"])
+            device.open()
+            device.load_merge_candidate(filename=f"config/ospf_{host["hostname"].lower()}.conf")
+            print(device.compare_config())
+            #apply the changes
+            device.commit_config()
 
 
 def question_34():
-    pass
+    with open("data/hosts.json", 'r') as hosts:
+        data = json.load(hosts)
+    for host in data:
+        driver = get_network_driver('ios')
+        device  = driver(hostname=host["ip"], username=host["username"], password=host["password"])
+        device.open()
+        config = device.get_config()
+        print(config["running"])
+        with open(f"config/{host["hostname"].upper()}.bak", 'w') as f:
+            f.write(config["running"])
+            f.close()
+
+
 
 
 
@@ -65,8 +97,8 @@ if __name__ == "__main__":
     #question_26(device)
     #question_27(device)
     #question_28(device)
-    question_29(device)
+    #question_29(device)
     #question_30(device)
     #question_31()
     #question_32()
-    #question_34()
+    question_34()
